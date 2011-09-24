3d8eb360cea6cf5eb2ee7a65035ad8085f72a453
lamdt <lamdt@lamdt-notebook-ubuntu1104.(none)>
2011-09-24 14:59:51 +0700

    (02:59:40 PM) hvnsweeting: git commit thôi (02:59:45 PM) hvnsweeting: git commit (02:59:45 PM) i_ke_love: gõ lung tung mà

---
+++ C/hello.c
@@ -0,0 +1,27 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  hello.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  09/24/2011 02:57:35 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  yoyolove (tunglam), traitimbanggia.199x@gmail.com
+ *        Company:  free
+ *
+ * =====================================================================================
+ */
+
+#include <stdio.h>
+
+int main()
+{
+	printf("\n Hello World");
+
+	return 0;
+
+}
