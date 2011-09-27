bcc530ccc08ffa14b1a1b6f0ae9b1eeff238027b
Viet Hung Nguyen <famihug@gmail.com>
2011-09-27 21:34:03 +0700

    use BufferedWriter, newLine(), add test with arr

--- Java/WriteToFile.java
+++ Java/WriteToFile.java
@@ -1,15 +1,24 @@
 /**
  * @author: FamiHug
  */
-import java.io.*:
+import java.io.*;
 
 public class WriteToFile 
 {
 	public static void main(String[] args)
 	{
 		try{
-			BufferedWriter out = new BufferedWriter(new File("out.txt"));
+			//create file ad BufferedWriter obj
+			BufferedWriter out = new BufferedWriter(new FileWriter("out.txt"));
 			out.write("FAMILUG");
+			out.newLine();//write \n
+			out.write("20110927");
+			out.newLine();//write \n
+
+			//Test with write a array of int
+			int[] arr = {1, 2, 3, 4, 5, 4, 3, 2, 1};
+			for(int i : arr)
+				out.write(i + " ");
 			//close output stream
 			out.close();
 		} catch(IOException e)
