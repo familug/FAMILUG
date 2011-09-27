580e61ae6aa199d2e948d07282adf9b391fe496c
Viet Hung Nguyen <famihug@gmail.com>
2011-09-27 20:39:14 +0700

    beautify code

--- HelloThon.java
+++ HelloThon.java
@@ -1,8 +1,10 @@
+/**
+ * @author: FamiHug@familug.com
+ */
+
 public class HelloThon {
 	public static void main (String [] args)
 	{
 		System.out.println("Hello thon");
-	}
-
-
-}
+	}//main()
+}//
