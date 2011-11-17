17671fba7894b53425698dcaf5828f303ec6571c
Viet Hung Nguyen <famihug@gmail.com>
2011-11-18 00:50:03 +0700

    fix variable err

--- Bash/shutdown.sh
+++ Bash/shutdown.sh
@@ -2,7 +2,7 @@
 #This script help autoshutdown your computer
 #To run this, change it to executable
 #use "chmod a+x shutdown.sh"
-$PASS = "MatKhau"
+PASS="MatKhau"
 
 #sudo -S is use input from stdin
 echo $PASS | sudo -S shutdown -h now
