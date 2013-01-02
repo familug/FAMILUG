be9980ecf3c0e89fdd186fa92a0ffd566b84d659
HVN <hvnsweeting@gmail.com>
2013-01-02 23:19:56 +0700

    add reduce and reject

--- ruby/pe001.rb
+++ ruby/pe001.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 
 class PE001
-    def solve
+    def solve_python
         sum = 0
         (0..999).each do |n| 
             if n % 5 == 0 || n % 3 == 0
@@ -11,8 +11,13 @@ class PE001
         return sum
     end
 
+    def solve_reduce
+        return (0..999).reject{|i| i % 3 != 0 and i % 5 !=0}.reduce(:+)
+    end
+
     def show_result
-        puts solve
+        puts solve_python
+        puts solve_reduce
     end
 end
 
