16a4aeae36e482a802d446927832bea9f2d1a8ea
at VCC <lamdt@vccloud.vn>
2013-07-12 16:19:08 +0700

    move file

--- lua/fingerprint.lua
+++ lua/fingerprint.lua
@@ -1,1 +1,23 @@
-/home/lamdt/lua/fingerprint.lua
+victim = arg[1]
+SEP = "\n+------------------------------------+"
+
+print(">> Fingerprinting "..victim)
+print(SEP)
+
+
+handle = io.popen("ping -c 3 "..victim)
+result = handle:read("*a")
+
+print(">> PING: "..string.match(result, "%w%% packet loss"))
+print(SEP)
+
+print("Scanning open port...")
+
+handle = io.popen("nmap -sT "..victim.." | grep http")
+result = handle:read("*a")
+
+if string.find(result,"80/tcp") ~= nil then
+    print(">> WARNING 80 is OPEN")
+else
+    print(">> Port 80 closed")
+end
