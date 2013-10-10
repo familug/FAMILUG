e44c54fe473b79d9b47461347934a819fdb747e4
hvnsweeting <hvnsweeting@gmail.com>
2013-10-11 01:43:47 +0700

    add hello thon example

---
+++ Guile/hellothon.scm
@@ -0,0 +1,11 @@
+#!/usr/bin/guile \
+-e main -s
+!#
+
+; by HVNSweeting <hvn@familug.org>
+
+(define (main args)
+  (display (+ 1 2 3 4 5)) (newline)
+  (display "Hellow thon") (newline)
+  (display "From FAMILUG - Fri Oct 11 01:36:43 ICT 2013") (newline)
+)
