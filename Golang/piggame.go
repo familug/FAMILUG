067d255b2a3591fa5ca986209d1fa97042d6ffda
Hung Nguyen Viet <hvn@familug.org>
2015-07-05 15:10:16 +0700

    use golang naming convention

--- Golang/piggame.go
+++ Golang/piggame.go
@@ -7,7 +7,7 @@ import (
 )
 
 const (
-	score_to_win_a_game = 100
+	scoreToWinAGame = 100
 )
 
 type Player struct {
@@ -22,32 +22,32 @@ func (p *Player) hold(score int) bool {
 	return score > 10
 }
 
-func (p *Player) play(user_score, turn_total int) int {
-	turn_score := p.roll()
-	log.Printf("%s rolls: %d\n", p.Name, turn_score)
-	if turn_score == 1 {
-		return user_score
+func (p *Player) play(userScore, turnTotal int) int {
+	turnScore := p.roll()
+	log.Printf("%s rolls: %d\n", p.Name, turnScore)
+	if turnScore == 1 {
+		return userScore
 	} else {
-		turn_total += turn_score
-		if p.hold(turn_total) {
+		turnTotal += turnScore
+		if p.hold(turnTotal) {
 			log.Printf("%s holds", p.Name)
-			return user_score + turn_total
+			return userScore + turnTotal
 		} else {
-			return p.play(user_score, turn_total)
+			return p.play(userScore, turnTotal)
 		}
 	}
 }
 
-func play_a_game(p1, p2 Player) (winner Player, turns int) {
-	p1_score, p2_score := 0, 0
+func playGame(p1, p2 Player) (winner Player, turns int) {
+	p1Score, p2Score := 0, 0
 	for i := 0; ; i++ {
-		p1_score = p1.play(p1_score, 1)
-		p2_score = p2.play(p2_score, 1)
-		log.Printf("Foo: %d - Bar: %d", p1_score, p2_score)
-		if p1_score >= score_to_win_a_game {
+		p1Score = p1.play(p1Score, 1)
+		p2Score = p2.play(p2Score, 1)
+		log.Printf("Foo: %d - Bar: %d", p1Score, p2Score)
+		if p1Score >= scoreToWinAGame {
 			return p1, i
 		}
-		if p2_score >= score_to_win_a_game {
+		if p2Score >= scoreToWinAGame {
 			return p2, i
 		}
 	}
@@ -57,15 +57,15 @@ func main() {
 	rand.Seed(time.Now().UnixNano())
 	p1 := Player{"Foo"}
 	p2 := Player{"Bar"}
-	winner, turns := play_a_game(p1, p2)
+	winner, turns := playGame(p1, p2)
 	log.Printf("%s won after %d turns.", winner.Name, turns)
-	p1_wins := 0
+	p1Wins := 0
 	nogame := 100
 	for i := 0; i < nogame; i++ {
-		winner, _ := play_a_game(p1, p2)
+		winner, _ := playGame(p1, p2)
 		if winner == p1 {
-			p1_wins += 1
+			p1Wins += 1
 		}
 	}
-	log.Printf("%s won %d, %s won %d", p1.Name, p1_wins, p2.Name, nogame-p1_wins)
+	log.Printf("%s won %d, %s won %d", p1.Name, p1Wins, p2.Name, nogame-p1Wins)
 }
