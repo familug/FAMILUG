308d184c4e3eb3c306651d22a3a909f6810db5f1
Hung Nguyen Viet <hvn@familug.org>
2017-02-26 12:46:25 +0700

    Refactor, turn to generator

--- Python/major_scale.py
+++ Python/major_scale.py
@@ -16,29 +16,28 @@ WHOLE = 2
 HALF = 1
 
 
-def next_steps(note, steps=HALF):
-    idx = notes.index(note)
+def next_note(start_note, steps=HALF):
+    idx = notes.index(start_note)
     for step in range(steps):
         idx = (idx + 1) % noteno
     return notes[idx]
 
 
 def major_scale(start):
-    out = []
-    out.append(start)
     n = start
+    yield n
     for step in [WHOLE, WHOLE, HALF, WHOLE, WHOLE, WHOLE, HALF]:
-        n = next_steps(n, step)
-        out.append(n)
-
-    return out
+        n = next_note(n, step)
+        yield n
 
 
 def main():
     argp = argparse.ArgumentParser()
     argp.add_argument('note', help='Note starts the major scale')
     args = argp.parse_args()
-    print(major_scale(args.note))
+    for note in major_scale(args.note):
+        print(note, end=' ')
+    print()
 
 
 if __name__ == "__main__":
