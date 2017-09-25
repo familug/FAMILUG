ab3f32b8b0cd0fc82ed8edbda13587fc4a6fdb8f
Viet Hung Nguyen <hvn@familug.org>
2017-09-25 10:44:55 +0700

    Add time passed and start of pomodoro

--- Python/pomodoro.py
+++ Python/pomodoro.py
@@ -1,16 +1,27 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import datetime
 import subprocess as spr
 import time
 
 
 def main():
+    start = datetime.datetime.now()
     spr.call(['notify-send', 'Started new pomodoro'])
     time.sleep(30 * 60)
+    end = datetime.datetime.now()
+    duration = (end - start).total_seconds() // 60
+
     for i in range(5):
         time.sleep(3)
-        spr.call(['notify-send', 'POMODORO END'])
+        spr.call(
+            ['notify-send',
+             'POMO: {0:.0f} minute passed.\tFrom {1}'.format(
+                 duration,
+                 start.strftime("%H:%M:%S"))
+             ]
+        )
 
 
 if __name__ == "__main__":
