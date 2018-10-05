69962abfc7e06c48a8ce8f49db842154fa11ed73
Viet Hung Nguyen <hvn@familug.org>
2018-10-05 09:07:53 +0700

    Add guide on list, add count persons

--- ChickenScheme/README.md
+++ ChickenScheme/README.md
@@ -1,3 +1,15 @@
 # Scheme
 
 https://pp.pymi.vn/article/scm1/
+
+## List
+
+### Creating
+- '(a b c) : List of symbol a b c
+- '(1 2 a b) : List of symbol 1 2 a b - 1 2 also is number
+- (list 1 2 'a 'b) : Same as above
+
+### Modifying
+#### Add element to head of list
+- (cons 1 '(a b c 2)) => (1 a b c 2) : CONStructing new list with 1 and the second list's elements
+- (append '(a b c) '(2 3 4)) => (a b c 2 3 4) : create new list by concat two lists.
--- ChickenScheme/tree_child.scm
+++ ChickenScheme/tree_child.scm
@@ -9,6 +9,15 @@
               (or (born-2000-ancestor? (person-father one))
                   (born-2000-ancestor? (person-mother one))))]))
 
+(define (count-persons a-tree)
+  (cond
+    [(null? a-tree) 0]
+    [else (+ 1
+             (count-persons (person-father a-tree))
+             (count-persons (person-mother a-tree))
+             )]))
+
+
 (define ChiPheo (make-person '() '() 'Chi 1990))
 (define ThiNo   (make-person '() '() 'No  2000))
 (define VangAnh (make-person ChiPheo ThiNo 'Anh 2010))
@@ -16,3 +25,5 @@
 (print (born-2000-ancestor? VangAnh))
 (print (born-2000-ancestor? ChiPheo))
 (print (born-2000-ancestor? '()))
+
+(print (count-persons VangAnh))
